5e4f7022752551c681ad5e55fb50abb9ddb16785
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2012-10-25 10:38:53 +0200

    nouveau component

---
+++ src/main/java/com/financeactive/training/components/ValueField.java
@@ -0,0 +1,17 @@
+package com.financeactive.training.components;
+
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jvedikunnel
+ * Date: 25/10/12
+ * Time: 10:36
+ */
+public class ValueField {
+
+    @Parameter
+    @Property
+    private String value;
+}
