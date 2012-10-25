99577d9ba36e4eb6eca1298e706f4e49291f568f
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2012-10-25 11:43:23 +0200

    Affichage et ajout du delete

--- src/main/java/com/financeactive/training/pages/users/IndexUsers.java
+++ src/main/java/com/financeactive/training/pages/users/IndexUsers.java
@@ -1,11 +1,32 @@
 package com.financeactive.training.pages.users;
 
+import com.financeactive.training.entities.User;
+import org.apache.tapestry5.EventConstants;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jvedikunnel
  * Date: 24/10/12
  * Time: 17:34
- * To change this template use File | Settings | File Templates.
  */
 public class IndexUsers {
+
+    @Inject
+    Session hbSession;
+
+    public List<User> getUsers() {
+        return hbSession.createCriteria(User.class).list();
+    }
+
+    @OnEvent(value = EventConstants.ACTION, component = "delete")
+    @CommitAfter
+    void onDelete(User user) {
+        hbSession.delete(user);
+    }
 }
