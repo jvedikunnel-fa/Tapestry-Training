1c091b17b88309638b412a134611885bc6edeca3
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2012-10-25 12:37:37 +0200

    Edit et create sur la même page

--- src/main/java/com/financeactive/training/pages/users/IndexUsers.java
+++ src/main/java/com/financeactive/training/pages/users/IndexUsers.java
@@ -2,7 +2,11 @@ package com.financeactive.training.pages.users;
 
 import com.financeactive.training.entities.User;
 import org.apache.tapestry5.EventConstants;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.PageActivationContext;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Session;
@@ -20,6 +24,13 @@ public class IndexUsers {
     @Inject
     Session hbSession;
 
+    @Property
+    @PageActivationContext
+    private User user;
+
+    @InjectComponent
+    private Zone formZone;
+
     public List<User> getUsers() {
         return hbSession.createCriteria(User.class).list();
     }
@@ -28,5 +39,11 @@ public class IndexUsers {
     @CommitAfter
     void onDelete(User user) {
         hbSession.delete(user);
+    }
+
+    @OnEvent(value = EventConstants.ACTION, component = "edit")
+    Zone onEdit(User user) {
+        this.user = user;
+        return formZone;
     }
 }
