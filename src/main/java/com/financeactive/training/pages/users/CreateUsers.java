71ab71ff3cdb0416cf8b7812aced08f1625db804
Vedikunnel Joseph <jvedikunnel@financeactive.com>
2012-10-25 11:30:28 +0200

    Commit de la création

--- src/main/java/com/financeactive/training/pages/users/CreateUsers.java
+++ src/main/java/com/financeactive/training/pages/users/CreateUsers.java
@@ -4,6 +4,9 @@ import com.financeactive.training.entities.User;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,9 +19,13 @@ public class CreateUsers {
     @Property
     private User user;
 
+    @Inject
+    private Session hbSession;
+
     @OnEvent(EventConstants.SUCCESS)
+    @CommitAfter
     private Class onSuccess() {
-        System.out.println("user : " + user);
+        hbSession.save(user);
         return IndexUsers.class;
     }
 }
